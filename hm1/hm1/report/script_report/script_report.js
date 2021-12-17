9eda4ad56dd4dccb4703f7f9b017ef525f5b0029
PassionateNaveenkumar <naveen.kumar@thirvusoft.in>
2021-12-17 12:21:01 +0530

    filter created

---
+++ hm1/hm1/report/script_report/script_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, naveen and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Script Report"] = {
+	"filters": [
+
+	]
+};
